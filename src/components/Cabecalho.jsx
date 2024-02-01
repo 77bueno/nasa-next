b86122463fef34d902b94caec7dbd1f0cdc66beb
victor <victorhugomotabueno@gmail.com>
2024-02-01 18:41:25 -0300

    Alterações

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -7,43 +7,48 @@ export default function Cabecalho() {
   return (
     <StyledHeader>
       <div className="limitador">
-            <Menu />
-          <h1>
-            <Link className="logo" href="/">
-                <Image
-                src="/images/nasa-logo.svg"
-                width={55}
-                height={55}
-                alt="Logo NASA"
-                />
-            </Link>
-          </h1>
-
-          <Link className="lupa" href="/contato">
-            <Image 
-              src="/images/icon-lupa.svg"
-              width={44}
-              height={44}
-              alt="Icone Lupa" 
+      <Link className="lupa" href="/contato">
+          <Image
+            src="/images/icon-lupa.svg"
+            width={35}
+            height={35}
+            alt="Icone Lupa"
+          />
+        </Link>
+        <h1 className="logo">
+          <Link href="/">
+            <Image
+              src="/images/nasa-logo.svg"
+              width={55}
+              height={55}
+              alt="Logo NASA"
             />
           </Link>
+        </h1>
+
+        <Menu />
       </div>
     </StyledHeader>
   );
 }
 
 const StyledHeader = styled.header`
-    background-color: black;
+  background-color: black;
 
-    div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-    a {
-        text-decoration: none;
-        color: white;
-    }
-    
+  .logo {
+    display: flex;
+    align-items: center;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
 `;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,21 +2,41 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export default function Menu() {
-
   return (
     <StyledNav>
-
-        <h2><a href="" class="icone"> &equiv; </a></h2>
-
-   
-            <Link href="/">Blog</Link>
-            <Link href="/sobre">Sobre Nós</Link>
-            <Link href="/contato">Contate-nos</Link>
-            <Link href="/live">NASA <span>LIVE</span></Link>
+      <Link href="/">Blog</Link>
+      <Link href="/sobre">Sobre Nós</Link>
+      <Link href="/contato">Contate-nos</Link>
+      <Link href="/live">NASA+<span className="live">LIVE</span></Link>
     </StyledNav>
   );
 }
 
 const StyledNav = styled.nav`
-    
+  display: flex;
+  align-items: center;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+
+  a {
+    margin-right: 20px;
+    text-decoration: none;
+    color: white;
+    font-weight: bold; /* Adicione negrito para dar mais destaque aos links */
+    transition: color 0.3s ease; /* Adicione uma transição suave na cor */
+  }
+
+  .live {
+    margin-left: 5px;
+    border: solid 1px red;
+    font-weight: 300;
+    font-size: 13px;
+    padding: 2px;
+    border-radius: 2px;
+
+    &:hover {
+      background-color: red;
+    }
+  }
+
+  
 `;
