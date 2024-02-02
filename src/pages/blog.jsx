560c39f42090c418380b31c3b3f57033a3c908a9
victor <victorhugomotabueno@gmail.com>
2024-02-02 11:00:07 -0300

    Página blog

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -74,6 +74,20 @@ export default function Blog() {
 
     <StyledNoticia2>
       <h2>Explore o Universo a partir da sua Caixa de Entrada</h2>
+      <p>Mantenha-se atualizado(a) sobre as últimas notícias da NASA – da Terra à Lua, do Sistema Solar e além.</p>
+      <br />
+      <p>Nós nunca compartilharemos seu endereço de e-mail. <span>Política Privada.</span></p>
+      <h3 className="botaoSt">
+          Inscrever-se
+          <Link href="/imagens">
+          <Image
+            className="botaoSeta"
+            src="/images/botao-seta.svg"
+            width={23} height={23}
+            alt="Botão com uma seta"
+          />
+          </Link>
+      </h3>
     </StyledNoticia2>
   </>
   )
@@ -133,6 +147,10 @@ const StyledNoticia = styled.section`
     padding: 1rem 0 1rem 0;
   }
 
+  p {
+    line-height: 1.3rem;
+  }
+
   .botaoSeta {
     margin: 0.5rem 0 -0.2rem 1rem;
   }
@@ -156,10 +174,34 @@ const StyledNoticia = styled.section`
 `;
 
 const StyledNoticia2 = styled.section`
-color: #fff;
+  color: #fff;
   background-image: linear-gradient(to right, black, transparent), url("images/nasa-galaxia2.png");
   background-size: cover;
-  height: 100vh;
+  height: 50vh;
   background-repeat: no-repeat;
   padding: 0 0 0 2rem;
+
+  h2 {
+    font-size: 2rem;
+    padding: 4rem 0 3rem 0;
+  }
+
+  p {
+    line-height: 1.5rem;
+    font-size: 1.2rem;
+    padding: 0 2rem 0 0;
+  }
+
+  .botaoSeta {
+    margin: 0.5rem 0 -0.2rem 1rem;
+  }
+
+  .botaoSt {
+    font-size: 18px;
+    margin: 1rem 0;
+  }
+
+  span {
+    text-decoration: underline;
+  }
 `;
